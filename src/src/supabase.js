1a6d910862e2964f15c2eeaa4bb6a9eabb8724fe
Charlesabouezzi <93452589+Charlesabouezzi@users.noreply.github.com>
2026-04-26 21:39:19 -0400

    Delete src/src/supabase.js

--- src/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://aggsqxorkzhnfdrctajy.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_Bxl0HyvWH74dmlIcBWM7AQ_srjLppDH';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
